dafc2bb619825b1c0888b4f5a5b9a368c2389ee0
Manish Chauhan <quickomanish@gmail.com>
2016-11-21 12:26:36 +0530

    test app

---
+++ server.js
@@ -0,0 +1,19 @@
+﻿    // Load required packages
+    var express = require('express');
+    var bodyParser = require("body-parser");
+
+
+
+    // Create our Express application
+    var app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ "extended" : false }));
+
+    var fs = require("fs");
+    // Default routing 
+    app.use(express.static(__dirname + '/'));
+
+
+
+    // Start the server
+    app.listen(8080);
